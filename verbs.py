1d4d4e28dbe405cf375c92fb65aa57697eee577f
jacpagan <jacpagan1@gmail.com>
2018-10-31 18:43:22 -0700

    best file yet

--- verbs.py
+++ verbs.py
@@ -6,20 +6,16 @@ import pprint
 from unidecode import unidecode
 import argparse
 
-verbs=["acabar", "andar", "aprender"] #"caer", "caerse", "cantar", "comenzar", "comer", "comprar", "conducir", "conocer", "construir", "contar", "creer", "dar", "deber", "decir", "dormir", "entrar", "escribir", "estar", "estudiar", "gustar", "haber", "hablar", "hacer", "ir", "irse","jugar", "leer", "llamar", "llamarse", "llevar", "mirar", "mirarse", "oír", "pagar", "pensar", "perder", "poder", "poner", "ponerse", "quedarse", "querer", "saber", "salir", "sentir", "sentirse", "ser", "tener", "tomar", "traer", "venir", "ver", "vivir", "volver"]
+verbs=["acabar", "andar", "aprender", "caer", "caerse", "cantar", "comenzar", "comer", "comprar", "conducir", "conocer", "construir", "contar", "creer", "dar", "deber", "decir", "dormir", "entrar", "escribir", "estar", "estudiar", "gustar", "haber", "hablar", "hacer", "ir", "irse","jugar", "leer", "llamar", "llamarse", "llevar", "mirar", "mirarse", "oír", "pagar", "pensar", "perder", "poder", "poner", "ponerse", "quedarse", "querer", "saber", "salir", "sentir", "sentirse", "ser", "tener", "tomar", "traer", "venir", "ver", "vivir", "volver"]
 
-# 'dañar',
 v_dict = {}
 
-# Iterate through the verbs
 for verb in verbs:
     url = "http://www.spanishdict.com/verbos/{}".format(unidecode(verb))
-    print(url)
     html = urlopen(url)
     bsObj = BeautifulSoup(html, "html.parser")
+    print(url)
 
-
-    #grab all the verb tenses on the page
     tenses = set()
     for sibling in bsObj.findAll( "div",{"class":"vtable-word-text"}):
         tenses.add(sibling.attrs['data-tense'])
