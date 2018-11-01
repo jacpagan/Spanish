4456975cd398c6243eddd7896cd2f0cbdf8a1bda
jacpagan <jacpagan1@gmail.com>
2018-10-31 18:14:06 -0700

    adds to the dictionary. fixing append situation

--- verbs.py
+++ verbs.py
@@ -4,30 +4,35 @@ from bs4 import BeautifulSoup
 import json
 import pprint
 from unidecode import unidecode
+import argparse
 
-def make_verbs(verbs):
-    for verb in verbs:
-        url = "http://www.spanishdict.com/verbos/{}".format(unidecode(verb))
-        html = urlopen(url)
-        soup = BeautifulSoup(html, "html.parser")
-        tenses = set()
-    
-        for link in soup.find_all('a'): 
-            if (link.get("data-tense")):
-                tenses.add(link.get("data-tense"))
+verbs=["acabar", "andar", "aprender"] #"caer", "caerse", "cantar", "comenzar", "comer", "comprar", "conducir", "conocer", "construir", "contar", "creer", "dar", "deber", "decir", "dormir", "entrar", "escribir", "estar", "estudiar", "gustar", "haber", "hablar", "hacer", "ir", "irse","jugar", "leer", "llamar", "llamarse", "llevar", "mirar", "mirarse", "oír", "pagar", "pensar", "perder", "poder", "poner", "ponerse", "quedarse", "querer", "saber", "salir", "sentir", "sentirse", "ser", "tener", "tomar", "traer", "venir", "ver", "vivir", "volver"]
 
-        t_dict = {}
-        for tense in tenses:
-            t_list = []
-            for sibling in soup.findAll( "a",{"data-tense":"{}".format(tense)}):
-                t_list.append(sibling.text)
-            t_dict[tense] = t_list
+# 'dañar',
+v_dict = {}
 
-        v_dict = {}
-        v_dict[verb] = t_dict
+# Iterate through the verbs
+for verb in verbs:
+    url = "http://www.spanishdict.com/verbos/{}".format(unidecode(verb))
+    print(url)
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html, "html.parser")
 
-verbs=["acabar", "andar", "aprender", "caer", "caerse", "cantar", "comenzar", "comer", "comprar", "conducir", "conocer", "construir", "contar", "creer", "dar", "deber", "decir", "dormir", "entrar", "escribir", "estar", "estudiar", "gustar", "haber", "hablar", "hacer", "ir", "irse","jugar", "leer", "llamar", "llamarse", "llevar", "mirar", "mirarse", "oír", "pagar", "pensar", "perder", "poder", "poner", "ponerse", "quedarse", "querer", "saber", "salir", "sentir", "sentirse", "ser", "tener", "tomar", "traer", "venir", "ver", "vivir", "volver"]
+
+    #grab all the verb tenses on the page
+    tenses = set()
+    for sibling in bsObj.findAll( "div",{"class":"vtable-word-text"}):
+        tenses.add(sibling.attrs['data-tense'])
+
+    t_dict = {}
+    for tense in tenses:
+        t_list = []
+        for sibling in bsObj.findAll( "div",{"data-tense":"{}".format(tense)}):
+            t_list.append(sibling.text)
+        t_dict[tense] = t_list
+
+    v_dict[verb] = t_dict
+
 
 with open('verbs.json', 'w') as outfile:
-    json.dump((json.dumps(make_verbs(verbs), sort_keys=True, ensure_ascii=False)), outfile, sort_keys=True, ensure_ascii=False)
-
+    json.dump((json.dumps(v_dict, sort_keys=True, ensure_ascii=False)), outfile, sort_keys=True, ensure_ascii=False)
